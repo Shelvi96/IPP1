7742e597433652f8f3387a513ad7d3dac6a39e12
Shelvi96 <shelvi9696@gmail.com>
2018-08-15 15:02:54 +0200

    Final version.

--- test.sh
+++ test.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-for i in test/*.in; do
-	f="${i%.*}"
-	./main <$f.in >wyj.out 2>wyj.err
-	if diff wyj.out $f.out >& /dev/null && diff wyj.err $f.err >& /dev/null; then
-		echo -e $i ": OK"
+for i in $2/*.in; do
+	file="${i%.*}"
+
+	valgrind --error-exitcode=15 --leak-check=full --show-leak-kinds=all --errors-for-leak-kinds=all ./$1 <$file.in >& /dev/null
+	exitcode=$?
+
+	./$1 <$file.in >wyj.out 2>wyj.err
+
+	if diff wyj.out $file.out >& /dev/null && diff wyj.err $file.err >& /dev/null && [ $exitcode == 0 ]; then
+		echo -e $i ": \e[32m OK \e[39m"
 	else
-		echo -e $i ": dupa";
-		break;
+		if [ $exitcode == 15 ]; then
+			echo -e $i ": \e[31m MEM \e[39m"
+		else
+			echo -e $i ": \e[31m WA \e[39m"
+		fi
 	fi
+
 	# read -p "Press any key to continue... " -n1 -s
 done;
 
--- utilities.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILITIES_H
-#define UTILITIES_H
-
-treeTree* errorTree;
-
-void emergencyExit ();
-
-#endif /* UTILITIES_H */
